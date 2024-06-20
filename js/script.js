d2235a203ad6fc720ad43ea610bc0de6b5d8ee94
George Artemov <oscurasgeorge@gmail.com>
2024-06-20 09:24:38 +0100

    slider update

--- js/script.js
+++ js/script.js
@@ -208,3 +208,122 @@ new Menu(
   ".menu .container",
   "menu__item"
 ).createMenuCard();
+
+
+// Slider js доработанная более сложная версия
+const slides = document.querySelectorAll('.offer__slide')
+const sliderImg = document.querySelectorAll('.offer__slide img')
+const nextSlideBtn = document.querySelector('.offer__slider-next')
+const prevSlideBtn = document.querySelector('.offer__slider-prev')
+const sliderCounter = document.querySelector('.offer__slider-counter span')
+const slidesWrapper = document.querySelector('.offer__slider-wrapper')
+const slidesField = document.querySelector('.offer__slider-inner')
+const width = window.getComputedStyle(slidesWrapper).width
+const currentSlideNum = document.querySelector('#current')
+const totalSlideNum = document.querySelector('#total')
+
+let slideIndex = 1
+let offset = 0
+
+
+if (slides.length < 10) {
+  totalSlideNum.textContent = `0${slides.length}`
+  currentSlideNum.textContent = `0${slideIndex}`
+} else {
+  totalSlideNum.textContent = slides.length
+  currentSlideNum.textContent = slideIndex
+}
+
+slides.forEach(slide => slide.style.width = width)
+
+slidesField.style.width = 100 * slides.length + '%'
+slidesField.style.display = 'flex'
+slidesField.style.transition = '0.5s all'
+
+slidesWrapper.style.overflow = 'hidden'
+
+nextSlideBtn.addEventListener('click', () => {
+  if (offset == +width.slice(0, width.length - 2) * (slides.length -1)) {
+    offset = 0
+  } else {
+    offset += +width.slice(0, width.length - 2)
+  }
+
+  slidesField.style.transform = `translateX(-${offset}px)`
+
+  if (slideIndex === slides.length) {
+    slideIndex = 1
+  } else {
+    slideIndex++
+  }
+
+  if (slides.length < 10) {
+    currentSlideNum.textContent = `0${slideIndex}`
+  } else {
+    currentSlideNum.textContent = slideIndex
+  }
+  
+})
+
+prevSlideBtn.addEventListener('click', () => {
+  if (offset == 0) {
+    offset = +width.slice(0, width.length - 2) * (slides.length -1)
+  } else {
+    offset -= +width.slice(0, width.length - 2)
+  }
+
+  slidesField.style.transform = `translateX(-${offset}px)`
+
+  if (slideIndex === 1) {
+    slideIndex = slides.length
+  } else {
+    slideIndex--
+  }
+  
+  if (slides.length < 10) {
+    currentSlideNum.textContent = `0${slideIndex}`
+  } else {
+    currentSlideNum.textContent = slideIndex
+  }
+  
+})
+
+
+// showSlides(slideIndex)
+
+// function showSlides(index) {
+//   if (index > sliderBox.length) {
+//     slideIndex = 1
+//   }
+
+//   if (index < 1) {
+//     slideIndex = sliderBox.length
+
+//   }
+
+//   sliderBox.forEach(slide => slide.style.display = 'none')
+//   sliderBox[slideIndex - 1].style.display = 'block'
+  
+//   if (slideIndex < 10) {
+//     sliderCounter.innerHTML = '0' + slideIndex
+//   }
+  
+// }
+
+//   function nextSlide(index) {
+//     showSlides(slideIndex += index)
+//   }
+
+//   function prevSlide(index) {
+//     showSlides(slideIndex += index)
+//   }
+
+//   nextSlideBtn.addEventListener('click', () => {
+//     nextSlide(1)
+//   })
+
+//   prevSlideBtn.addEventListener('click', () => {
+//     prevSlide(-1)
+//   })
+
+
